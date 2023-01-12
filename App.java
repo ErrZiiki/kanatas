c30374f243b701586a4d3322b74773fb4305d2fd
Diego de la Fuente <diegodelafuentealonso@gmail.com>
2023-01-12 12:12:30 +0100

    practicando con git push

--- App.java
+++ App.java
@@ -4,6 +4,9 @@ public class App {
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("Diego de la Fuente Alonso");
+    System.out.println("12/1/2023");
+    System.out.println("León");
   }
   
 }
